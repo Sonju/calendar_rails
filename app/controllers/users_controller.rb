cab399c7441ed52ef0c4209fb0c122b9e81e2e0a
Sonju Walker <sonjuwalker@gmail.com>
2015-08-16 16:26:59 -0500

    Table Users RENAMED password TO password_dgest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,27 @@ class UsersController < ApplicationController
     # render a view and lets user sigin
   end
 
-# pos
+# post
   def confirmation
+    # puts '-----users#controller-------'
+    # puts user_params
+    # puts '-----end users#controller-------'
     # post reguest that handles regiration and says thanks
+
+    @user = User.new(user_params)
+    if @user.save
+      # thanks your in
+      session[:user_id] = @user_id
+    else
+      @message = 'User account does exist or password and email do not match.'
+      render 'user/signup'
+      # woomp woomp
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest, :password_confirmation)
   end
 end
