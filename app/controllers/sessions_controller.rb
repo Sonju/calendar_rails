3e982a6ff0efc39500bdf5c93f1d2512f4c09bb7
Sonju Walker <sonjuwalker@gmail.com>
2015-08-17 09:55:36 -0500

    still trying to fix error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,16 +9,26 @@ class SessionsController < ApplicationController
   def confirmation
     puts '-----sessions#controller-------'
 
-    puts '-----end sessios#controller-------'
+    puts '-----end sessions#controller-------'
 
     # shows user login status
     @user = User.find_by_email(params[:email])
   end
+    # if @user && @user.authenticatie(params[:password])
+    #   session[:user_id] = @user.id
+    #   redirect_to '/'
+
+    # else
+    #   @message = 'User account or password do not match'
+    #   render '/sessions/login'
+  #   end
+  #
+  # end
 
 # get
   def logout
     # user logout
     session[:user_id] = nil
-    redirect_to 'users/login'
+    redirect_to '/users/login'
   end
 end
